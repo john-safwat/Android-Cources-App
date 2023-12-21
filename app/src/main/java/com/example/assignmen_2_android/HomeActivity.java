c71ad3a0bacb64dd173f8a309d1fd2c7923b3e57
John Safwat <john362sss@gmail.com>
2023-12-21 23:40:34 +0200

    create splash Screen
    + Delete themes.xml
    + Add activity_home.xml
    + Add HomeActivity.java
    + Add kotlinc.xml
    + Add splash_screen.png
    + Modify activity_splash.xml
    + Modify AndroidManifest.xml
    + Modify build.gradle.kts
    + Modify build.gradle.kts
    + Modify colors.xml
    + Modify discord.xml
    + Modify misc.xml
    + Modify SplashActivity.java
    + Modify strings.xml
    + Modify themes.xml

---
+++ app/src/main/java/com/example/assignmen_2_android/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.assignmen_2_android;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
--- app/src/main/java/com/example/assignmen_2_android/SplashActivity.java
+++ app/src/main/java/com/example/assignmen_2_android/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignmen_2_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        // to navigate from splash screen to home in 3000 milliseconds time
+        new Handler().postDelayed(() -> {
+            // Start your app main activity
+            Intent intent = new Intent(this , HomeActivity.class);
+            startActivity(intent);
+            // close this activity
+            finish();
+        }, 3000);
+
     }
 
 }
